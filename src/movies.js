4088c4027f7084fd5785e8d509f00cf3b92e2813
Roberto Lopez-Trigo <rlope232@fiu.edu>
2021-01-28 17:54:18 -0500

    Bob

--- src/movies.js
+++ src/movies.js
@@ -53,58 +53,69 @@ function ratesAverage(myArr) {
 
 function dramaMoviesRate(array) {
   let filteredDramaMovies = array.filter((eachMovie) => {
-    return eachMovie.genre.includes('Drama')
-  })
+    return eachMovie.genre.includes("Drama");
+  });
 
-  return ratesAverage(filteredDramaMovies)
+  return ratesAverage(filteredDramaMovies);
 }
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
 function orderByYear(array) {
-  console.log(array)
+  console.log(array);
   const result = array.sort((a, b) => {
     if (a.year > b.year) {
       return 1;
     } else if (b.year > a.year) {
       return -1;
     } else {
-
       return a.title.localeCompare(b.title);
     }
-  })
+  });
   return [...result];
 }
-
-
-
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(arr) {
   //sort but title
   let sortedArray = [...arr].sort((a, b) => {
-    return (a.title.localeCompare(b.title)) //it is either 0,1,-1
+    return a.title.localeCompare(b.title); //it is either 0,1,-1
+  });
 
+  let first20titles = sortedArray.slice(0, 20).map((item) => {
+    return item.title;
+  });
 
-  })
-
-  let first20titles = sortedArray.slice(0, 20).map(item => {
-    return item.title
-  })
-
-  return first20titles
+  return first20titles;
   // return first 20 items
 }
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 
-let testString = "1h 36min"
-function turnHoursToMinutes(testString) {
-  let hours = parseInt(testString)
-  return hours
+let testString = "1h 36min";
+function turnHoursToMinutes(arr) {
+  let newArr = arr.map((movie) => {
+    let eachMovie = {...movie};
+    let words = eachMovie.duration.split(" ");
+    let hours = parseInt(words[0]);
+    if (words[0].includes('m'))
+    {
+      eachMovie.duration = parseInt(words[0])
+    } else {
+    
+    let minutes = 0;
+    if (words[1]) {
+      minutes = parseInt(words[1]);
+    }
 
+    let totalTime = hours * 60 + minutes;
+    eachMovie.duration = totalTime;
+  }
+    return eachMovie;
+  });
+  console.log(arr)
+  console.log(newArr)
+  return newArr;
 }
-
-
 
 // BONUS - Iteration 8: Best yearly rate average - Best yearly rate average
